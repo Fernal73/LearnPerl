f1a9ddb42389175275e6203bf322e2d547338f99
fernal73 <linus.fernandes@gmail.com>
2019-10-26 05:06:15 +0530

    Added simple program

---
+++ simple.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+use strict;
+# this program reads a line of input, and writes the line back out
+my $inputline = <STDIN>; 
+# read a line of input
+print( $inputline ); 
+# write the line out
