8d4151416d40b7f25f04b1a4273e3a039f998d0d
fernal73 <linus.fernandes@gmail.com>
2019-10-26 15:31:58 +0530

    sizing

---
+++ size.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my $value = 1234567890;
+print ("first value is ", $value, "\n");
+$value = 1234567890123456;
+print ("second value is ", $value, "\n");
+$value = 12345678901234567890;
+print ("third value is ", $value, "\n");
+$value = 12345678901234567890;
+print ("fourth value is ", $value, "\n");
+$value = 123456789012345678901234567890;
+print ("fifth value is ", $value, "\n");
