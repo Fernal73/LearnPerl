1471a57e7e4ba23eec750e0c6fc3fed7f82442cf
fernal73 <linus.fernandes@gmail.com>
2019-10-26 05:48:01 +0530

    Non zero perl script

---
+++ nonzero.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+print ("Enter a number:\n");
+my $number = <STDIN>;
+chop ($number);
+if ($number) {
+  print ("The number is not zero.\n");
+}
+print ("This is the last line of the program.\n");
