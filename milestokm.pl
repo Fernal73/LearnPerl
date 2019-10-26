50d54963a190bd0ae7d79901744fef84061ffccc
fernal73 <linus.fernandes@gmail.com>
2019-10-26 05:38:26 +0530

    Added more scripts

---
+++ milestokm.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+print ("Enter the distance to be converted:\n");
+my $originaldist = <STDIN>;
+chop ($originaldist);
+my $miles = $originaldist * 0.6214;
+my $kilometers = $originaldist * 1.609;
+print ($originaldist, " kilometers = ", $miles," miles\n");
+print ($originaldist, " miles = ", $kilometers," kilometers\n");
