729b8cc9e2daa3916a9797e62741507b5775e412
fernal73 <linus.fernandes@gmail.com>
2019-10-26 16:17:50 +0530

    Tidied.

--- size.pl
+++ size.pl
@@ -2,12 +2,12 @@
 use strict;
 use warnings;
 my $value = 1234567890;
-print ("first value is ", $value, "\n");
+print( "first value is ", $value, "\n" );
 $value = 1234567890123456;
-print ("second value is ", $value, "\n");
+print( "second value is ", $value, "\n" );
 $value = 12345678901234567890;
-print ("third value is ", $value, "\n");
+print( "third value is ", $value, "\n" );
 $value = 12345678901234567890;
-print ("fourth value is ", $value, "\n");
+print( "fourth value is ", $value, "\n" );
 $value = 123456789012345678901234567890;
-print ("fifth value is ", $value, "\n");
+print( "fifth value is ", $value, "\n" );
--- until.pl
+++ until.pl
@@ -9,11 +9,11 @@ print("What is 17 plus 26?\n");
 my $correct_answer = 43;
 my $input_answer   = <STDIN>;
 chop($input_answer);
-printf("Number input: %f\n", $input_answer);
+printf( "Number input: %f\n", $input_answer );
 until ( $input_answer == $correct_answer ) {
   print("Wrong! Keep trying!\n");
   $input_answer = <STDIN>;
   chop($input_answer);
-  printf("Number input: %f\n", $input_answer);
+  printf( "Number input: %f\n", $input_answer );
 }
 print("You've got it!\n");
