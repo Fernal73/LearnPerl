cd0374a0a87d8bed64eff802702a80d419df6f84
fernal73 <linus.fernandes@gmail.com>
2019-10-26 20:24:09 +0530

    Fixed for untidiness

--- equals.pl
+++ equals.pl
@@ -5,11 +5,11 @@ our $VERSION = 1.0;
 print "Enter a number:\n";
 my $number1 = <>;
 chop $number1;
-printf "Number 1:%f\n",$number1;
+printf "Number 1:%f\n", $number1;
 print "Enter another number:\n";
 my $number2 = <>;
 chop $number2;
-printf "Number 2:%f\n",$number2;
+printf "Number 2:%f\n", $number2;
 
 if ( $number1 == $number2 ) {
   print "The two numbers are equal.\n";
--- nonzero.pl
+++ nonzero.pl
@@ -5,7 +5,7 @@ our $VERSION = 1.0;
 print "Enter a number:\n";
 my $number = <>;
 chop $number;
-printf "Number: %f\n",$number;
+printf "Number: %f\n", $number;
 
 if ($number) {
   print "The number is not zero.\n";
