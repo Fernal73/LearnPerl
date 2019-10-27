494013b3093ba4fab7c17a8c1c84b26f29611405
fernal73 <linus.fernandes@gmail.com>
2019-10-28 04:57:16 +0530

    sorting

---
+++ bubbleint.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+our $VERSION = '1.00';
+
+# read the array from standard input one item at a time
+print "Enter the array to sort, one item at a time.\n";
+my $count     = 1;
+my $inputline = <>;
+chop $inputline;
+my @array;
+while ( $inputline ne q{} && $inputline ne '' ) {
+  @array[$count - 1] = $inputline;
+  $count++;
+  $inputline = <>;
+  if ( defined($inputline) ) {
+    chop $inputline;
+  }
+  else {
+    last;
+  }
+}
+
+# now sort the array
+$count = 1;
+my $x;
+while ( $count < @array ) {
+  $x = 1;
+  while ( $x < @array ) {
+    if ( $array[$x - 1] > $array[$x] ) {
+      @array[$x - 1, $x] = @array[$x, $x - 1];
+    }
+    $x++;
+  }
+  $count++;
+}
+
+# finally, print the sorted array
+print "@array\n";
