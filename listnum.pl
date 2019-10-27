d82742eb09c798eafd25ae3734aec7d3a67cb2e8
fernal73 <linus.fernandes@gmail.com>
2019-10-27 19:46:10 +0530

    List numbers. perl critic fixes

---
+++ listnum.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+our $VERSION = '1.00';
+print "Enter the start number:\n";
+my $start = <>;
+chop $start;
+print "Enter the end number:\n";
+my $end = <>;
+chop $end;
+my @list  = ( $start .. $end );
+my $count = 0;
+my $size  = scalar @list;
+
+if ( $size == 0 ) {
+  exit 0;
+}
+print "Here is the list:\n";
+while ( $count < $size ) {
+  print "$list[$count]\n";
+  $count++;
+}
