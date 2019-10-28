7d83a2f0550d99c07d7ad78c411ea5162593d43e
fernal73 <linus.fernandes@gmail.com>
2019-10-28 13:21:13 +0530

    modified for perl critic

--- printfile.pl
+++ printfile.pl
@@ -1,14 +1,16 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Carp qw( croak );
 our $VERSION = '1.00';
 my $line;
 my $file = 'words.txt';
-if ( open my $fh, $file or die "Could not open $file: $!" ) {
+if ( open my $fh, q{<}, $file or croak "Could not open $file: $!" ) {
   $line = <$fh>;
   while ( $line ne q{} && $line ne q{} ) {
     print $line;
     last if eof;
     $line = <$fh>;
   }
+  close $fh or croak "unable to close: $!";
 }
