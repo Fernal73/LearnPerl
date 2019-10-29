3bd5df7293bb76fcfee9b678431634b8cbb1c389
fernal73 <linus.fernandes@gmail.com>
2019-10-29 19:45:56 +0530

    Fixed for warnings

--- isdate.pl
+++ isdate.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 our $VERSION = '1.00';
 
-## no critic (ProhibitMagicNumbers)
+## no critic (ProhibitMagicNumbers,)
 
 sub is_leap_year {
   my ($year) = @_;
@@ -33,6 +33,13 @@ sub is_29_feb {
   return;
 }
 
+sub get_year {
+  my ($date) = @_;
+  if ( $date =~ /([\d]+)/mxs ) {
+    return $1;
+  }
+}
+
 # handle 31-day months
 my $md1 = '(0[13578]|1[02])\\2(0[1-9]|[12]\\d|3[01])';
 
@@ -46,7 +53,6 @@ my $date;
 my $match;
 my $olddate;
 while ( $date = <> ) {
-  no warnings 'numeric';
   last if !defined $date;
   chomp $date;
 
@@ -60,7 +66,7 @@ while ( $date = <> ) {
       print "$date is a valid date\n";
       next;
     }
-    if ( is_29_feb($date) && is_leap_year( int $date ) ) {
+    if ( is_29_feb($date) && is_leap_year( get_year($date) ) ) {
       print "$date is a valid date\n";
       next;
     }
@@ -71,7 +77,7 @@ while ( $date = <> ) {
       print "$date is a valid date but not in the 20th century.\n";
       next;
     }
-    if ( is_29_feb($date) && is_leap_year( int $date ) ) {
+    if ( is_29_feb($date) && is_leap_year( get_year($date) ) ) {
       print "$date is a valid date but not in the 20th century.\n";
       next;
     }
