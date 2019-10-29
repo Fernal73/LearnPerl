24244fbd1a885e8063263145ec4d8be1fb4bad00
fernal73 <linus.fernandes@gmail.com>
2019-10-29 14:10:11 +0530

    isemail

---
+++ isemail.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+our $VERSION = '1.00';
+my $email;
+while ( $email = <> ) {
+  last if !defined $email;
+  chomp $email;
+  if ( $email =~ /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/mxs ) {
+    print "$email is a legal email address.\n";
+  }
+  else {
+    print "$email is not a legal email address.\n";
+  }
+}
