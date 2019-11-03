33182956a644f275645f6ed0d47a1f9cb7fbba6b
fernal73 <linus.fernandes@gmail.com>
2019-11-03 18:12:43 +0530

    count vowels

--- print.pl
+++ print.pl
@@ -1,15 +1,13 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use English qw(-no_match_vars);
-use IO::Handle;
 use Perl6::Form;
 
 our $VERSION = '1.00';
 
 my $text = form
-'===================================',
-'Here is the text I want to display.',
-'===================================';
+  '===================================',
+  'Here is the text I want to display.',
+  '===================================';
 
 print $text;
---
+++ vowels.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Perl6::Form;
+
+our $VERSION = '1.00';
+
+my @vowels;
+my %vowelcount = qw(a 0 e 0 i 0 o 0 u 0);
+while ( my $line = <> ) {
+  $line =~ s/[^aeiou]//igsmx;
+  @vowels = split //msx, $line;
+  foreach my $vowel (@vowels) {
+    $vowelcount{$vowel} += 1;
+  }
+}
+
+my $format = form
+  '==========================================================',
+  'Number of vowels found in text file:',
+  "a: {<<<<<} e: {<<<<<}",
+  $vowelcount{'a'}, $vowelcount{'e'},
+  "i: {<<<<<} o: {<<<<<}",
+  $vowelcount{'i'}, $vowelcount{'o'},
+  "u: {<<<<<}",
+  $vowelcount{'u'},
+  '==========================================================';
+
+print $format;
